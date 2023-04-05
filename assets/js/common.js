c6966a1ffdd0aca5b78e248fe019d5dd2126c148
Reza Hartono <rezahartono11@gmail.com>
2023-04-05 12:30:33 +0700

    update hero 05/04/2023

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,15 @@
+var stickyElement = $('.sticky-top');
+
 $(document).ready(function () {
     // for disable open dev tools
     // disableConsole()
 })
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        stickyElement.addClass('shadow');
+    } else {
+        stickyElement.removeClass('shadow');
+    }
+});
