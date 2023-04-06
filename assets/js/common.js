e99d7266b9070a298ef1853b32de74452211e561
Reza Hartono <rezahartono11@gmail.com>
2023-04-06 08:33:07 +0700

    update contact us 05/04/2023

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,4 @@
-var stickyElement = $('.sticky-top');
+var stickyElement = $('.sticky-top')
 
 $(document).ready(function () {
     // for disable open dev tools
@@ -11,9 +11,9 @@ $(document).ready(function () {
 
 $(window).scroll(function () {
     if ($(window).scrollTop() > 0) {
-        stickyElement.addClass('shadow');
+        stickyElement.addClass('shadow')
     } else {
-        stickyElement.removeClass('shadow');
+        stickyElement.removeClass('shadow')
     }
 });
 
@@ -21,6 +21,32 @@ var splide = new Splide('.splide', {
     perPage: 3,
     focus: 0,
     omitEnd: true,
-});
+})
 
-splide.mount();
+splide.mount()
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz9Nf_nkzKCJV-89P8RUiDNqY32iqhAz3UhKQme2Oxs6bUY-qy8ub_AdYtWn3Fvd7gaKA/exec'
+const form = document.forms['contact-us-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response = () => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Pesan kamu berhasil di kirimkan',
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+        })
+        .catch(error => console.error('Error!', error.message))
+        .finally(() => {
+            $(':input', '#contact-us-form')
+                .not(':button, :submit, :reset, :hidden')
+                .val('')
+                .prop('checked', false)
+                .prop('selected', false)
+        })
+})
+
